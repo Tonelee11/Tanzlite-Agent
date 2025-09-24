9768a1e04fc5b6ce57e6b0c559011732a703efe4
Anthony Charles <124351714+Tonelee11@users.noreply.github.com>
2025-09-24 13:55:40 +0300

    Update webchat.js

--- webchat.js
+++ webchat.js
@@ -497,7 +497,7 @@
         }
 
         .chat-assist-widget .user-registration {
-            position: absolute;
+            position: relative;
             top: 50%;
             left: 50%;
             transform: translate(-50%, -50%);
